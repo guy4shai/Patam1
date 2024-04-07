f6a1e2e2fb5f6300c846bbb777ecbdf514243289
guy4shai <guyhazannnn@gmail.com>
2024-04-07 16:37:05 +0300

    changes

--- src/test/Tile.java
+++ src/test/Tile.java
@@ -3,5 +3,23 @@ package test;
 
 public class Tile {
 
-	
+	public final char letter;
+    public final int score;
+    private Tile(char letter, int score){
+        this.letter = letter;
+        this.score = score;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public class Bag {
+        
+    }
+
 }
